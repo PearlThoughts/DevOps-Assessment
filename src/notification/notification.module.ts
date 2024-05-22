a20e7a1af583f3920873be9d32fc6a51786f6af6
apschauhan210 <apschauhan181@gmail.com>
2024-05-22 22:06:36 +0530

    removed irrelevant files

--- src/notification/notification.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmailController } from './controllers/email/email.controller';
-import { EmailService } from './services/email/email.service';
-
-@Module({
-  controllers: [EmailController],
-  providers: [EmailService]
-})
-export class NotificationModule {}
