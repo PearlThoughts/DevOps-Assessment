c9fc379a511129fd72637fccce2e71596d066862
ratnakottara <ratnakottara@gmail.com>
2024-06-01 02:49:54 +0530

    Implemented CI/CD

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,25 +1,3 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "notification-cluster"
-}
-
-resource "aws_ecs_task_definition" "task" {
-  family                   = "notification-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-
-  container_definitions = jsonencode([
-    {
-      name      = "notification"
-      image     = "${aws_ecr_repository.notification.repository_url}:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
-    }
-  ])
+resource "aws_ecs_cluster" "mycluster" {
+  name = "notification_cluster"
 }
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,8 @@
 resource "aws_ecr_repository" "notification_service" {
-  name                 = "notification-service"
+  name = "notification-service"
+
   image_tag_mutability = "MUTABLE"
+
   image_scanning_configuration {
     scan_on_push = true
   }
