dcea18bdf869eb03426d865a7d9051c27afd541a
Akash-1704 <90324028+Akash-1704@users.noreply.github.com>
2024-06-01 07:50:09 +0530

    Update cluster.tf

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -2,15 +2,4 @@ resource "aws_ecs_cluster" "ecs-cluster" {
   name = var.aws_ecs_cluster_name
 }
 
-data "template_file" "demo_ecs_app" {
-  template = file("./templates/demo_ecs_app.json.tpl")
-  vars = {
-    app_image      = aws_ecr_repository.demo_ecs_app.repository_url
-    app_port       = var.app_port
-    fargate_cpu    = var.fargate_cpu
-    fargate_memory = var.fargate_memory
-    tag            = var.tag
-    name           = var.aws_ecr_repository
-  }
-}
 
