3f7a1084ccf95ba38c2c7da7aac5323a5c7acbe4
Bhushan chougale <bhushanchougale162@gmail.com>
2024-06-02 14:36:05 +0530

    Update secrets-manager.tf

---
+++ terraform/secrets-manager.tf
@@ -0,0 +1,10 @@
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "db-password"
+}
+
+resource "aws_secretsmanager_secret_version" "db_password" {
+  secret_id     = aws_secretsmanager_secret.db_password.id
+  secret_string = jsonencode({
+    password = "my-secret-password"
+  })
+}
