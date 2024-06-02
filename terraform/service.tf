6217c854b5b4690d582200af9138767ce5cddcab
ratnakottara <ratnakottara@gmail.com>
2024-06-02 18:27:12 +0530

    api redirection

--- terraform/service.tf
+++ terraform/service.tf
@@ -61,12 +61,57 @@ resource "aws_lb_listener" "notification_lb_listener" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
-  
+
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.notification_tg.arn
   }
+
+  dynamic "default_action" {
+    for_each = var.forwarding_rules
+    content {
+      type = default_action.value.type
+
+      redirect {
+        port        = default_action.value.redirect.port
+        protocol    = default_action.value.redirect.protocol
+        host        = default_action.value.redirect.host
+        path        = default_action.value.redirect.path
+        query       = default_action.value.redirect.query
+        status_code = default_action.value.redirect.status_code
+      }
+    }
+  }
 }
+
+variable "forwarding_rules" {
+  description = "Forwarding rules for ALB listener"
+  type = list(object({
+    type     = string
+    redirect = object({
+      port        = string
+      protocol    = string
+      host        = string
+      path        = string
+      query       = string
+      status_code = string
+    })
+  }))
+  default = [
+    {
+      type = "redirect"
+      redirect = {
+        port        = "80"
+        protocol    = "HTTP"
+        host        = var.load_balancer_dns_name
+        path        = "/api"
+        query       = "#{query}"
+        status_code = "HTTP_301"
+      }
+    }
+  ]
+}
+
 
 resource "aws_ecs_service" "notification_service" {
   name            = "notification-service"
